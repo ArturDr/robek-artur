49c34821f3b1de5189033cd204747d1cb73128ef
Artur Robek <robek.artur@gmial.com>
2022-10-25 22:02:02 +0200

    add hero & plugins

--- wp-content/themes/robek-artur/header.php
+++ wp-content/themes/robek-artur/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,34 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'robek-artur' ); ?></a>
+	<?php wp_body_open();
+	get_template_part('template-parts/hero'); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$robek_artur_description = get_bloginfo( 'description', 'display' );
-			if ( $robek_artur_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $robek_artur_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'robek-artur' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+	<nav class="navbar navbar-expand-md artur-nav">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="<?php echo site_url('strona-glowna'); ?>">Robek Artur</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="justify-content-center collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('/blog'); ?>">Blog</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('/o-mnie'); ?>">O mnie</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('/oferta'); ?>">Oferta</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo get_post_type_archive_link('realizacje'); ?>">Realizacje</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('/kontakt'); ?>">Kontakt</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	</header><!-- #masthead -->
---
+++ wp-content/themes/robek-artur/template-parts/hero.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Robek_Artur
+ */
+
+?>
+<div class='container-fluid'>
+<h1> hero section </h2>
+</div>
